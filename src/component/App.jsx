b6669d96f5e7610b962181d38089970f263f5c82
VISHALSHARMAVS <vishalsharmakt143@gamil.com>
2023-12-28 23:52:35 +0530

    primary Commit : Keeper App using React

---
+++ src/component/App.jsx
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./footer";
+function App() {
+  return <div>
+    <Header></Header>
+    <Note></Note>
+    <Footer></Footer>
+  </div>
+}
+
+export default App;
